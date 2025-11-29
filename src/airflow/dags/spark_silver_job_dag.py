d40e33b5caae766dd65aa542d118d2c6addaadcf
VND2004 <vunhuduc2004@gmail.com>
2025-11-29 09:20:22 +0700

    Fix gitignore for airflow folder 2

---
+++ src/airflow/dags/spark_silver_job_dag.py
@@ -0,0 +1,56 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id='spark_silver_job',
+    default_args=default_args,
+    description='Run Spark Silver job daily',
+    schedule_interval='0 2 * * *',
+    start_date=datetime(2025, 11, 1),
+    catchup=False,
+    tags=['spark', 'silver'],
+) as dag:
+
+    run_silver = SparkSubmitOperator(
+        task_id='run_silver_job',
+        application='/opt/airflow/src/silver.py',
+        conn_id='spark_default',
+        deploy_mode='client',
+        spark_binary='spark-submit',
+        executor_cores=2,
+        executor_memory='1g',
+        driver_memory='512m',
+        total_executor_cores=2,
+        verbose=True,
+        name='silver-job',
+        conf={
+            'spark.jars.packages': ','.join([
+                'org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1',
+                'org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.0',
+                'org.apache.hadoop:hadoop-aws:3.3.4',
+                'com.amazonaws:aws-java-sdk-bundle:1.12.767',
+            ]),
+            'spark.sql.catalog.lakehouse': 'org.apache.iceberg.spark.SparkCatalog',
+            'spark.sql.catalog.lakehouse.type': 'hadoop',
+            'spark.sql.catalog.lakehouse.warehouse': 's3a://lakehouse/warehouse',
+            'spark.hadoop.fs.s3a.endpoint': 'http://minio:9000',
+            'spark.hadoop.fs.s3a.access.key': 'admin',
+            'spark.hadoop.fs.s3a.secret.key': '12345678',
+            'spark.hadoop.fs.s3a.path.style.access': 'true',
+            'spark.hadoop.fs.s3a.impl': 'org.apache.hadoop.fs.s3a.S3AFileSystem',
+            'spark.hadoop.fs.s3a.aws.credentials.provider': 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider',
+            'spark.hadoop.fs.s3a.connection.ssl.enabled': 'false',
+            'spark.sql.extensions': 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions',
+        },
+    )
+
+    run_silver
