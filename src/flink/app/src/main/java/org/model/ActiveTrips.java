e8fa6b9c5c0dee0e220e5a94027b22b34a14dae3
Duc <ducngoquang275@gmail.com>
2025-12-23 16:40:55 +0700

    Bug fix and Read me modification

--- src/flink/app/src/main/java/org/model/ActiveTrips.java
+++ src/flink/app/src/main/java/org/model/ActiveTrips.java
@@ -5,50 +5,35 @@ import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
 
 import java.io.Serializable;
+import java.util.Date; // Crucial import
 
 @Table(keyspace = "realtime", name = "active_trips")
 public class ActiveTrips implements Serializable {
     
     @PartitionKey
     @Column(name = "window_start")
-    public long windowStart;
+    public Date windowStart; 
     
     @Column(name = "window_end")
-    public long windowEnd;
+    public Date windowEnd;   
     
     @Column(name = "active_trips")
     public long activeTrips;
 
-    // Zero-arg constructor required
     public ActiveTrips() {}
 
     public ActiveTrips(long windowStart, long windowEnd, long activeTrips) {
-        this.windowStart = windowStart;
-        this.windowEnd = windowEnd;
+        this.windowStart = new Date(windowStart);
+        this.windowEnd = new Date(windowEnd);
         this.activeTrips = activeTrips;
     }
 
-    public long getWindowStart() {
-        return windowStart;
-    }
+    public Date getWindowStart() { return windowStart; }
+    public void setWindowStart(Date windowStart) { this.windowStart = windowStart; }
 
-    public void setWindowStart(long windowStart) {
-        this.windowStart = windowStart;
-    }
+    public Date getWindowEnd() { return windowEnd; }
+    public void setWindowEnd(Date windowEnd) { this.windowEnd = windowEnd; }
 
-    public long getWindowEnd() {
-        return windowEnd;
-    }
-
-    public void setWindowEnd(long windowEnd) {
-        this.windowEnd = windowEnd;
-    }
-
-    public long getActiveTrips() {
-        return activeTrips;
-    }
-
-    public void setActiveTrips(long activeTrips) {
-        this.activeTrips = activeTrips;
-    }
+    public long getActiveTrips() { return activeTrips; }
+    public void setActiveTrips(long activeTrips) { this.activeTrips = activeTrips; }
 }
