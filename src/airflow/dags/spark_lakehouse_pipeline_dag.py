c59e48a37e07f347d673b05c0bffe7ba93ba4875
VND2004 <vunhuduc2004@gmail.com>
2025-12-27 09:21:00 +0700

    Rename and update DAG to run both Silver and Gold jobs in pipeline

--- src/airflow/dags/spark_lakehouse_pipeline_dag.py
+++ src/airflow/dags/spark_lakehouse_pipeline_dag.py
@@ -11,13 +11,13 @@ default_args = {
 }
 
 with DAG(
-    dag_id='spark_silver_job',
+    dag_id='spark_lakehouse_pipeline',
     default_args=default_args,
-    description='Run Spark Silver job daily',
+    description='Run Spark Silver and Gold jobs daily',
     schedule='0 2 * * *',
     start_date=datetime(2025, 11, 1),
     catchup=False,
-    tags=['spark', 'silver'],
+    tags=['spark', 'lakehouse', 'silver', 'gold'],
 ) as dag:
 
     run_silver = SparkSubmitOperator(
@@ -39,4 +39,23 @@ with DAG(
         },
     )
 
-    run_silver
+    run_gold = SparkSubmitOperator(
+        task_id='run_gold_job',
+        application='/opt/airflow/src/gold.py',
+        conn_id='spark_default',
+        deploy_mode='client',
+        spark_binary='spark-submit',
+        executor_cores=2,
+        executor_memory='1g',
+        driver_memory='512m',
+        total_executor_cores=2,
+        verbose=True,
+        name='gold-job',
+        packages='org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.0,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.767',
+        conf={
+            'spark.pyspark.python': 'python3',
+            'spark.pyspark.driver.python': 'python3',
+        },
+    )
+
+    run_silver >> run_gold
